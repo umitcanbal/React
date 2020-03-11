b36eca4b604fabbe652f4ab186e43fd9d5fbc082
umitcanbal <umitcanbal@gmail.com>
2020-03-11 17:16:23 +0100

    sadece önceki deneyleri temizledim, dosyayı ilk committeki hale getirdim

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,27 +5,15 @@ export default class App extends React.Component {
     constructor(props) {
         super(props); // calls the constructor of the extended class
                       // which is React.Component
-        console.log(this);
-        console.log(333);
-
-        // this.render() 
         
     }
 
     render() {
-        const myVariable = React.createElement("h3", null, "hey hey");
-        console.log(3311);
+        
         return(
             <div>
-                {/* <h3>Hey Hey!</h3> */}
-                {myVariable} 
-                {console.log(3311)}
-
-                {/* Üstteki iki satır tamamen aynı işi yapıyo browserda, yani ben <h3></h3> şeklinde html taginin içine bir şey koyunca aslında React.createElement fonksiyonunu çağırmış oluyorum, yani "<></>" direkt olarak fonksiyon başlatıyo ve arka planda aynı işi yapıyo. Bu iki satırdaki kodun da prop'ları var (denemek istersen "{console.log(myVariable)}" ve "{console.log(<h3>Helloo</h3)}" şeklinde yazıp sonuçlara bakabilirsin ), ve propları tamamen boş değil (ben "null" diye belirtmeme rağmen) props.children sekmesi dolu. Neyle dolu? içine yazdığımız düz yazıyla dolu, bunlar prop.children'a düşüyolar/yazılıyolar.   */}
-
-                {/* Mesela "console.log(myVariable.props.children)" bunu yazdığında sana direkt içine yazdığın metni veriyo, ama this.props.children diyemiyosun çünkü bu neye tekabül eder? Seni renderlayan (yani ReactDOM, yani index.jsx sayfası) komponentin içine yazdığın şeye, yani <App>XXX</App> buradaki this.probs.children = XXX olur, parenttan verdiğimiz herhangi bi prop gibi. "this" component instance'dır, "myVariable" react elementtir!  */}
+                <p>hellooo!</p>
             </div>
         )
     }
-
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,93 +7,11 @@ import './index.scss';
 import App from './Components/App.jsx';
 
 
-// let example = <div>alal</div>
-// let example2 = React.createElement("div", null, "alal");
-//*** Bunlar aynı şey
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+);
 
-
-// let a = App;
-// console.log("it is a", a);
-//*** Sadece class/fonksiyon/componentin tanımını yazdırır. Constructor'ı render'ı vs çağırmaz.
-
-
-// let b = <App/>
-// console.log("it is b", b);
-//*** Bu hiçbir şey göstermiyo, sadece react elementini gösteriyo, prop'un içi de bomboş??????????????????????????
-
-
-// let d = new App;
-// console.log("it is d", d);
-//*** Bu component'in construct methodunu tamamen çalıştırır, render'ı çalıştırmaz AMA sen this.render() diye constructor'ın içine yazarsan o zaman full her şeyi elde edersin AKSİ HALDE AŞAĞIDAKİ GİBİ...
-//*** EK NOT: Burada "d" aynı zamanda "this"'e eşit olur! Yani component instance
-
-
-// let e = d.render();
-// console.log("it is e", e);
-//*** Buradaki "e" react elementidir.
-
-
-// console.log(<new App/>);
-//*** Bu saçma sapan bi şey oldu
-
-
-
-
-// ReactDOM.render(
-//     <App/>,
-//     document.getElementById('app')
-// );
-
-
-
-
-// console.log(<App />);
-// console.log(React.createElement(App, null, "noluyo"));
-
-//Şu yukarıdaki iki satır tamamen aynı!! ÇOK ÖNEMLİ!
-
-// console.log(App); Bu ise çok farklı bi şey.
-
-// ReactDOM.render(
-//     React.createElement(App, null, "noluyo"),
-//     document.getElementById('app')
-// );
-//Böyle de renderlayabilirsin App'ı aynı şey oluyo. Ama browserda noluyo yazmaz, App'in içinde neyi returnlüyosan o yazar. Çünkü zaten genel olarak (galiba hiçbir zaman) biz this.props.children'a tekabül eden şeyi renderlamıyoruz ki, şimdi niye renderlayalım.
-// Eğer ki bu şekilde ekrana "noluyo"yu yazdırmak istiyosan, git App componentinde, return kısmına {this.props.children} de, o zaman yazdırırsın.
-
-
-
-
-//Bi websitesinden bulduğum açıklama
-//JSX is just a tool that converts files like this:
-
-// let element =
-// <div className='test'>
-//   Hello, world!
-// </div>
-// Into files like this:
-
-// let element =
-// React.createElement(
-//   'div',
-//   { className: 'test' },
-//   "Hello, world!"
-// )
-// To repeat myself, all JSX does is replace a file’s <tags> with equivalent calls to React.createElement().
-
-// JSX is not, not a special way to embed HTML in JavaScript - it’s not a template system. It’s just a tool for rewriting <tags> into calls to React.createElement().
-
-
-
-
-// 1-console.log(<App />);
-// 2-console.log(App);
-// 3-console.log(this); (App.jsx sayfasındaki constructor'ın içine yazıldı bu)
-
-// Bu üçü de farklı sonuçlar veriyo. 
-// BİRİNCİSİ; react element
-// İKİNCİSİ; class/fonksiyon/komponent
-// ÜÇÜNCÜSÜ; component instance
 
 
 
