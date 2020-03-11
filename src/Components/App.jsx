478b894bc1e00943533d5136a25c3c2c0afe0120
umitcanbal <umitcanbal@gmail.com>
2020-03-12 00:41:00 +0100

    tekrar temizledim

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -5,12 +5,6 @@ import React from 'react';
 
 export default class App extends React.Component {
 
-    constructor(prooops) {
-        super(prooops); // calls the constructor of the extended class
-                      // which is React.Component
-        
-    }
-
     render() {
         
         return(
