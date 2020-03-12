e46792e7a7bcbbef9b4fe672f1d190317d4e13ab
umitcanbal <umitcanbal@gmail.com>
2020-03-12 01:12:08 +0100

    functional component yarattım

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,14 +3,19 @@ import React from 'react';
 
 
 
-export default class App extends React.Component {
 
-    render() {
-        
+export const App = ({name}) => {
+
         return(
+            
             <div>
                 <p>heyy</p>
+                <p>{name}</p>
             </div>
-        )
-    }
+        );
 }
+
+export default App;
+
+
+//Eğer yukarıdaki gibi export yapmazsan da 7. satıra gidip "export const App" yazarsan çalışmıyo, niye bilmiyorum.
--- src/index.jsx
+++ src/index.jsx
@@ -16,7 +16,7 @@ class Main extends React.Component {
     render() {
         return(
             <div>
-                <App/>
+                <App name="ümitcan"/>
             </div>
         )
     }
