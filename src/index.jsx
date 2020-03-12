cb6c650120c6f73c88726d7edce42971b07ccc04
umitcanbal <umitcanbal@gmail.com>
2020-03-12 22:35:20 +0100

    lifecycle, mount and unmount(with a button), didmount, willmount etc.

--- src/index.jsx
+++ src/index.jsx
@@ -10,13 +10,33 @@ import App from './Components/App.jsx';
 class Main extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            homeMounted : true
+        }
 
     }
 
+    onChangeHomeMounted() {
+        this.setState({
+            homeMounted : !this.state.homeMounted
+        });
+    }
+
+    changeMounted = () => {
+        this.onChangeHomeMounted()
+    }
+
     render() {
+        let homeComponent = "";
+
+        if (this.state.homeMounted) {
+            homeComponent = <App name="ümitcan"/>
+        }
+        
         return(
             <div>
-                <App />
+                {homeComponent}
+                <button onClick={this.changeMounted}>Unmount me</button>
             </div>
         )
     }
